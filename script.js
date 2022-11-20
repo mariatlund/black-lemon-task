0c975d786992ad0df40e27a3a32c4b85f000d915
Maria Lundqvist <mari28rd@stud.kea.dk>
2022-11-20 16:40:23 +0100

    accordion functionality

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+// ACCORDION
+
+document.querySelectorAll(".accordion-item h2").forEach((element) => {
+  element.addEventListener("click", (e) => {
+    let accordion = e.target;
+    accordion.nextElementSibling.classList.toggle("hidden");
+  });
+});
+
+// const accordion = document.querySelectorAll(".accordion-item h2");
+// let i;
+
+// for (i = 0; i < accordion.length; i++) {
+//   accordion[i].addEventListener("click", () => {
+//     const text = this.parentNode;
+//     console.log(text);
+
+//     // if (text.style.display === "block") {
+//     //   text.style.display = "none";
+//     // } else {
+//     //   text.style.display = "block";
+//     // }
+//   });
+// }
